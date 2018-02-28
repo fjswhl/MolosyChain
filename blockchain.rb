f40d18e0f069d718eef94561dab973372635491f
fjswhl <340403800@qq.com>
2018-03-01 02:20:28 +0800

    mod

--- blockchain.rb
+++ blockchain.rb
@@ -1,9 +1,13 @@
 require 'digest'
+require 'pp'
+require 'websocket-eventmachine-client'
+require 'websocket-eventmachine-server'
+require 'json'
 
 class Block
   def initialize(index, previous_hash, timestamp, data, hash)
     @index = index
-    @previousHash = previous_hash
+    @previous_hash = previous_hash
     @timestamp = timestamp
     @data = data
     @hash = hash
@@ -21,7 +25,7 @@ def get_latest_block
 end
 
 def calculate_hash(index, previous_hash, timestamp, data)
-  Digest::SHA256.digest(
+  Digest::SHA256.hexdigest(
     index.to_s +
     previous_hash.to_s +
     timestamp.to_s +
@@ -43,4 +47,94 @@ end
 
 def is_valid_new_block?(new_block, previous_block)
   if previous_block.index + 1 != new_block.index
+    pp 'invalid index'
+    return false
+  elsif previous_block.hash != new_block.previous_hash
+    pp 'invalid previoushash'
+    return false
+  elsif calculate_hash_for_block(new_block) != new_block.hash
+    pp 'invalid hash: ' + calculate_hash_for_block(new_block) + ' ' + new_block.hash
+    return false
+  else
+    return true
+  end
+end
+
+def add_block(new_block)
+  if is_valid_new_block(new_block, get_latest_block)
+    blockchain.push(new_block)
+  end
+end
+
+sockets = []
+module MessageType
+  QUERY_LATEST = 0
+  QUERY_ALL = 1
+  RESPONSE_BLOCKCHAIN = 2
+end
+
+def write(ws, message)
+  ws.send(message.to_json)
+end
+
+def broadcast(message)
+  sockets.each do |ws|
+    write(ws, message)
+  end
+end
+
+def response_chain_msg
+  { :type => MessageType::RESPONSE_BLOCKCHAIN, :data => blockchain.to_json }
+end
+
+def response_latest_msg
+  { :type => MessageType::RESPONSE_BLOCKCHAIN, :data => [get_latest_block].to_json }
+end
+
+def query_all_msg
+  { :type => MessageType::QUERY_ALL }
+end
+
+def replace_chain(new_blocks)
+  
+end
+
+def handle_blockchain_response(message)
+  received_blocks = JSON.parse(message['data'].sort { |x, y| x - y })
+  latest_block_received = received_blocks[received_blocks.length - 1]
+  latest_block_held = get_latest_block
+  if latest_block_received.index > latest_block_held.index
+    if latest_block_held.hash == latest_block_received.previous_hash
+      blockchain.push(latest_block_received)
+      broadcast(response_latest_msg)
+    elsif received_blocks.length == 1
+      broadcast(query_all_msg)
+    else
+      replace_chain(received_blocks)
+    end
+  else
+    pp 'received blockchain is not longer than current blockchain. Do nothing'
+  end
+end
+
+def init_p2p_server
+  WebSocket::EventMachine::Server.start(:host => "0.0.0.0", :port => 8080) do |ws|
+    sockets.push(ws)
+
+    ws.onmessage do |data, type|
+      message = JSON.parse(data)
+      pp 'Received message' + message
+      case message['type']
+      when MessageType::QUERY_LATEST
+        write(ws, response_latest_msg)
+      when MessageType::QUERY_ALL
+        write(ws, response_chain_msg)
+      when MessageType::RESPONSE_BLOCKCHAIN
+
+    end
+
+    ws.onclose do
+      puts "Client disconnected"
+    end
+  end
 end
